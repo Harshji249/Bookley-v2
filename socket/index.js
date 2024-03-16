38aca4c3954c02dd9406f48f8b0138ac99a48ab7
Harshji249 <hs2622411@gmail.com>
2024-03-17 00:25:28 +0530

    env added

--- socket/index.js
+++ socket/index.js
@@ -1,49 +1,6 @@
-// const { Server } = require("socket.io");
-// const userId = {};
-// const io = new Server({
-//     cors: {
-//         origin: "*",
-//         methods: ["GET", "POST"],
-//     },
-// });
-
-// io.on('connection', (socket) => {
-//     if (socket.handshake.query.userId) {
-//         userId[socket.handshake.query.userId] = socket.id;
-//         console.log("Active Users:", userId);
-//     }
-//     console.log('User connected:', socket.id);
-
-//     socket.on('disconnect', () => {
-//         console.log('User disconnected:', socket.id);
-//     });
-
-
-//     socket.on("sendMessage", (message) => {
-//         console.log("receiveNewMessage", message);
-//         io.to(userId[message.receiver]).emit("receiveNewMessage", message);
-//     });
-
-
-//     socket.on("disconnect", () => {
-//         console.log("Disconnected:", socket.id);
-//         Object.keys(userId).forEach((el) => {
-//             if (userId[el] === socket.id) {
-//                 delete userId[el];
-//                 console.log("Active Users:", userId);
-//             }
-//         });
-//     });
-// });
-
-
-// io.listen(3005, () => {
-//     console.log("Socket.IO server is running on port 3005");
-// });
-
-
 const { Server } = require("socket.io");
-
+require('dotenv').config()
+const port = process.env.PORT
 const userId = {};
 const io = new Server({
     cors: {
@@ -86,6 +43,6 @@ io.on("connect", (socket) => {
     });
 });
 
-io.listen(3005, () => {
+io.listen(port, () => {
     console.log("Socket.IO server is running on port 3005");
 });
