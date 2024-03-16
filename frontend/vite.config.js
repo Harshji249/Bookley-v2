e13bad1638f8ddc30b562e39fdb3135997429980
Harshji249 <hs2622411@gmail.com>
2024-03-17 00:40:23 +0530

    opt change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
+  server: {
     strictPort: true,
-    port:10000,
+    port: 10000,
   }
-})
+});
