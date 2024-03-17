c8d5ccf9d59dd119e53c9906767ef0c8b43405b8
Harshji249 <hs2622411@gmail.com>
2024-03-17 13:54:00 +0530

    package changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [reactRefresh()],
