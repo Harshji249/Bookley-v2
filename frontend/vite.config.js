883525f4e97ed150841544a027547bca97631f3e
Harshji249 <hs2622411@gmail.com>
2024-03-17 14:26:23 +0530

    removed mn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     minify: 'terser',
   },
+  server: {
+    host : true,
+    strictPort: true,
+  }
 });
