4eae2a02f7f8c2f19354187cd6450dc16a995dce
Harshji249 <hs2622411@gmail.com>
2024-03-17 13:46:23 +0530

    mini

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host : true,
-    strictPort: true,
-  }
+  plugins: [reactRefresh()],
+  build: {
+    minify: 'terser',
+  },
 });
