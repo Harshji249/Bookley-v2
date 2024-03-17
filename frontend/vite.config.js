d04e31d800fd79c8fa5acf232583cd4980476e27
Harshji249 <hs2622411@gmail.com>
2024-03-17 14:49:46 +0530

    changednew

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import TerserPlugin from 'terser-webpack-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    require('rollup-plugin-replace')({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    require('rollup-plugin-commonjs')(),
-    require('rollup-plugin-terser')(),],
-  
+  plugins: [react()],
   build: {
     minify: 'terser',
-  },
-  optimization: {
-    minimizer: [new TerserPlugin({})],
   },
   server: {
     host : true,
     strictPort: true,
-    port : 10000
   }
 });
